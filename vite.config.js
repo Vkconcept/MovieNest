a34d82216a527b0ab5c455d7edb52741abaecffb
williams victor <williamsvikky76@gmail.com>
2025-09-05 19:20:33 -0700

    Fix Vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MovieNest/', // add this line
+  base: './', // relative paths work on Vercel
   plugins: [react()],
 });
